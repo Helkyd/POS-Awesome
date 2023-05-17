2e6817c20ae9ca3e00091bdb739b0af6a8b5fbb9
Helkyd <hcesar@gmail.com>
2023-05-17 07:45:52 +0100

    Update __init__.py

--- posawesome/__init__.py
+++ posawesome/__init__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-import frappe
+#import frappe
 
 __version__ = "3.5.1"
 
 
-def console(*data):
-    frappe.publish_realtime("toconsole", data, user=frappe.session.user)
+#def console(*data):
+#    frappe.publish_realtime("toconsole", data, user=frappe.session.user)
